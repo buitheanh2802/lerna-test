be153fb5c43b4fd4e74b05dda99cf5906ab2032f
Bui The Anh <theanhbui345@gmail.com>
2023-08-16 22:49:32 +0700

    222

--- packages/package2/src/app.jsx
+++ packages/package2/src/app.jsx
@@ -1,36 +1,31 @@
-import React, { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const App = () => {
 
-    const [showAnimate,setShowAnimate] = useState(false);
+    const [showAnimate, setShowAnimate] = useState(false);
 
-    const onClick = () => {
-        setShowAnimate(!showAnimate)
+    const list = {
+      visible: { opacity: 1 },
+      hidden: { opacity: 0 },
     }
-
-  return (
-    <div>
-        <button onClick={onClick}>Click show animates</button>
-      <AnimatePresence>
-        {showAnimate && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            hello animate
-          </motion.div>
-        )}
-      </AnimatePresence>
-      <motion.div
-      animate={{x: [0,70,0]}}
-      transition={{ease: 'backInOut',duration: 1}}
+    
+    const item = {
+      visible: { opacity: 1, x: 0 },
+      hidden: { opacity: 0, x: -100 },
+    }
+    
+    return (
+      <motion.ul
+        initial="hidden"
+        animate="visible"
+        variants={list}
       >
-        Test first animate
-      </motion.div>
-    </div>
-  );
+        <motion.li variants={item}>Item 1</motion.li>
+        <motion.li variants={item}>Item 2</motion.li>
+        <motion.li variants={item}>Item 3</motion.li>
+      </motion.ul>
+    )
 };
 
 export default App;
